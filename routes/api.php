8a545094b7863ef49206d03b3e0dcfaaac8c8e21
Mamba-dot <shahnewaz886@gmail.com>
2025-10-26 13:28:15 +0600

    Fix route definition for fixed assets in API routes

--- routes/api.php
+++ routes/api.php
@@ -45,7 +45,7 @@ Route::middleware('auth:sanctum', 'verified')->group(function () {
     Route::apiResource('product-categories', CategoryController::class);
     Route::apiResource('brands', BrandController::class);
     // Fixed Asset Route
-    Route::apiResource('assets', FixedAssetController::class);
+w    Route::apiResource('fixed-assets', FixedAssetController::class);
     // Asset Depreciation Route
     Route::apiResource('asset-depreciations', AssetDepreciationController::class);
     // Asset Disposal Route
